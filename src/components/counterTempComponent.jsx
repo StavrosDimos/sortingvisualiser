ce0678d90bc1d022ebda40b04874b1ee5404a4f5
StavrosDimos <45982680+StavrosDimos@users.noreply.github.com>
2020-11-11 19:27:34 +1100

    added bootstrap libraries

--- src/components/counterTempComponent.jsx
+++ src/components/counterTempComponent.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 class Counter extends Component {
     state = {
@@ -9,11 +11,13 @@ class Counter extends Component {
     render(){
         return (
             <React.Fragment>
-                <div>
+                
                 <div style={{display: "inline-block", width: this.state.width, height: this.state.height, backgroundColor: 'skyblue'}}>
                 </div>
+                <div>
                 Hell orld
                 </div>
+                <Button>This is a button</Button>
                 
             </React.Fragment>
             );
